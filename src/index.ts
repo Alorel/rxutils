bdaa2b340a09161633463a20e0df8a26268227d2
Arturas <a.molcanovas@gmail.com>
2019-08-11 15:10:37 +0100

    feat(util): Added a NOOP_OBSERVER constant

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import * as operators from './operators';
 
 export {intervalRandom} from './creators/intervalRandom';
 export {wasLogged, setDefaultLogger} from './operators/logError';
+export {NOOP_OBSERVER} from './util/NOOP_OBSERVER';
 
 export {operators};
