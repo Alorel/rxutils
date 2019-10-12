070e9d327de056094cecec946f2fd53ed47dc51d
Arturas Molcanovas <a.molcanovas@gmail.com>
2019-10-12 19:54:36 +0100

    feat(util): observify function

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,7 @@ export {wasLogged, setDefaultLogger} from './operators/logError';
 export {NOOP_OBSERVER} from './util/NOOP_OBSERVER';
 export {nextObserver} from './util/nextObserver';
 export {asyncMap} from './creators/asyncMap';
+export {observify} from './util/observify';
+export {ObservifyInput} from './types/ObservifyInput';
 
 export {operators};
