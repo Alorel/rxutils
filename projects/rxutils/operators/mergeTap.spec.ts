178da8d84e77bb662cea3466ce6af2a2e0cde115
Arturas <a.molcanovas@gmail.com>
2022-11-13 06:01:34 +0000

    test: Increase timeouts to stabilise switch/mergeTap tests

--- projects/rxutils/operators/mergeTap.spec.ts
+++ projects/rxutils/operators/mergeTap.spec.ts
@@ -1,32 +1,15 @@
 import {expect} from 'chai';
-import {delay, lastValueFrom, of, timer} from 'rxjs';
-import {map, startWith, toArray} from 'rxjs/operators';
+import {switchMergeTapTestCommon} from '../internal/switchMergeTap.spec';
 import {mergeTap} from './mergeTap';
-
-/* eslint-disable @typescript-eslint/no-magic-numbers */
 
 describe('operators/mergeTap', function () {
   let asyncTapped: string[];
   let all: string[];
 
   before(async () => {
-    asyncTapped = [];
-
-    const src$ = of('c').pipe(
-      delay(15),
-      startWith('b'),
-      delay(5),
-      startWith('a'),
-      mergeTap(v => timer(10).pipe(
-        map(() => {
-          asyncTapped.push(v);
-
-          return v.toUpperCase();
-        })
-      )),
-      toArray()
-    );
-    all = await lastValueFrom(src$);
+    const init = await switchMergeTapTestCommon(mergeTap);
+    asyncTapped = init.asyncTapped;
+    all = init.all;
   });
 
   it('Should leave inputs unchanged, not halt timed out inputs', () => {
