01f331eb8ee716cd16f1954c89851f4967730aa3
Arturas <a.molcanovas@gmail.com>
2022-11-13 06:01:34 +0000

    test: Increase timeouts to stabilise switch/mergeTap tests

---
+++ projects/rxutils/internal/switchMergeTap.spec.ts
@@ -0,0 +1,30 @@
+import {delay, lastValueFrom, of, timer} from 'rxjs';
+import {map, startWith, toArray} from 'rxjs/operators';
+import type {mergeTap, switchTap} from '../operators';
+
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+
+export async function switchMergeTapTestCommon(
+  op: typeof switchTap | typeof mergeTap
+): Promise<Record<'all' | 'asyncTapped', string[]>> {
+  const asyncTapped: string[] = [];
+
+  const src$ = of('c').pipe(
+    delay(50),
+    startWith('b'),
+    delay(5),
+    startWith('a'),
+    op(v => timer(25).pipe(
+      map(() => {
+        asyncTapped.push(v);
+
+        return v.toUpperCase();
+      })
+    )),
+    toArray()
+  );
+
+  const all = await lastValueFrom(src$);
+
+  return {all, asyncTapped};
+}
--- projects/rxutils/operators/switchTap.spec.ts
+++ projects/rxutils/operators/switchTap.spec.ts
@@ -1,32 +1,15 @@
 import {expect} from 'chai';
-import {delay, lastValueFrom, of, timer} from 'rxjs';
-import {map, startWith, toArray} from 'rxjs/operators';
+import {switchMergeTapTestCommon} from '../internal/switchMergeTap.spec';
 import {switchTap} from './switchTap';
-
-/* eslint-disable @typescript-eslint/no-magic-numbers */
 
 describe('operators/switchTap', function () {
   let asyncTapped: string[];
   let all: string[];
 
   before(async () => {
-    asyncTapped = [];
-
-    const src$ = of('c').pipe(
-      delay(15),
-      startWith('b'),
-      delay(5),
-      startWith('a'),
-      switchTap(v => timer(10).pipe(
-        map(() => {
-          asyncTapped.push(v);
-
-          return v.toUpperCase();
-        })
-      )),
-      toArray()
-    );
-    all = await lastValueFrom(src$);
+    const init = await switchMergeTapTestCommon(switchTap);
+    asyncTapped = init.asyncTapped;
+    all = init.all;
   });
 
   it('Should leave inputs unchanged, halt timed out inputs', () => {
